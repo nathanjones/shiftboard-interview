6171f97a8011265e46f4794b5108b8e3e9ead2f3
Nathan Jones <me@nathanrjones.com>
2016-12-07 22:48:16 -0600

    Implemented loading random person

--- app/src/main/java/com/nathanrjones/shiftboard/ui/main/MainActivity.java
+++ app/src/main/java/com/nathanrjones/shiftboard/ui/main/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -52,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements MainScreen.View {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        setTitle("People With Friends");
     }
 
     @Override
@@ -67,6 +71,25 @@ public class MainActivity extends AppCompatActivity implements MainScreen.View {
 
         if (presenter != null) {
             presenter.detachView();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_random:
+                presenter.showRandomPerson();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -116,11 +139,6 @@ public class MainActivity extends AppCompatActivity implements MainScreen.View {
     @Override
     public void showLoading() {
         Snackbar.make(container, "Loading...", LENGTH_SHORT).show();
-    }
-
-    @OnClick(R.id.person_image)
-    void onLoadNewPersonClicked() {
-        presenter.showRandomPerson();
     }
 
     /**
